306db387bcab39daa141bf5aea0ea765d29c7dda
geekbrains <chester_095@mail.ru>
2022-09-01 14:51:21 +0300

    перевел на MVVM

--- app/src/main/java/com/gb/stopwatch/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/gb/stopwatch/viewmodels/MainActivityViewModel.kt
@@ -1,4 +1,31 @@
 package com.gb.stopwatch.viewmodels
 
-class MainActivityViewModel {
+import com.gb.stopwatch.MainActivityContract
+import com.gb.stopwatch.domain.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+class MainActivityViewModel: MainActivityContract.ViewModel()
+{
+    override val timestampProvider = object : TimestampProvider {
+        override fun getMilliseconds(): Long {
+            return System.currentTimeMillis()
+        }
+    }
+
+    override val stopwatchListOrchestrator = StopwatchListOrchestrator(
+        StopwatchStateHolder(
+            StopwatchStateCalculator(
+                timestampProvider,
+                ElapsedTimeCalculator(timestampProvider)
+            ),
+            ElapsedTimeCalculator(timestampProvider),
+            TimestampMillisecondsFormatter()
+        ),
+        CoroutineScope(
+            Dispatchers.Main
+                    + SupervisorJob()
+        )
+    )
 }
